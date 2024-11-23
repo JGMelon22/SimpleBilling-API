0de5ad43ad57df56d7018d8688b79ef7f765f165
JGMelon22 <jgvasconcellos22@gmail.com>
2024-11-23 11:47:00 -0300

    develop base models

---
+++ SimpleBilling-API/Models/BillItem.cs
@@ -0,0 +1,11 @@
+namespace SimpleBilling_API.Models;
+
+public class BillItem
+{
+    public string? Name { get; set; }
+    public decimal Price { get; set; }
+    public byte Discount { get; set; }
+    public decimal AfterDiscountPrice { get; set; }
+    public int Quantity { get; set; }
+    public decimal TotalPrice { get; set; }
+}
---
+++ SimpleBilling-API/Models/Enums/Currency.cs
@@ -0,0 +1,11 @@
+namespace SimpleBilling_API.Models.Enums;
+
+public enum Currency
+{
+    Usd,
+    Egp,
+    Eur,
+    Cad,
+    Jpy,
+    Brl
+}
